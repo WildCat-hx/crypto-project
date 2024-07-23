1455c900c8198ad95c8b39157cf22378bddc12ca
WildCat-hx <salomecadillon46@gmail.com>
2024-07-23 18:57:13 +0200

    main : suppr fichier main.js --> deplacees dans methodes.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import './methodes.js';
-
-document.addEventListener("DOMContentLoaded", function() {
-    navbarWhite ? navbarWhite.addEventListener('scroll', navScroll) : console.error("navbarWhite is null");
-    emailVerif ? emailVerif.addEventListener('input', emailVerification) : console.error('emailVerif is null');
-});
