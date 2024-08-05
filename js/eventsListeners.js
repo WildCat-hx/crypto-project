e010e1668885b077596433420d147d63959869ad
WildCat-hx <salomecadillon46@gmail.com>
2024-08-05 18:32:56 +0200

    main : suppression d'imports qui ne servent plus et nouveau event listener qui gere la partie click sur l'overlay et appelle la methode closeLoginModal

--- js/eventsListeners.js
+++ js/eventsListeners.js
@@ -1,16 +1,25 @@
 import { 
     navbarWhite, 
     loginButton, 
-    loginModal,
     closeModal, 
     scrollFunct, 
-    showLoginModal, 
-    hideLoginModal, 
-} from './methodes.js';
+    toggleLoginModal,
+    closeLoginModal,
+    overlay
+} from './methodes.js'
 
 document.addEventListener("DOMContentLoaded", function() {
     navbarWhite ? window.addEventListener('scroll', scrollFunct) : console.error("navbarWhite is null");
-    loginButton ? loginButton.addEventListener('click', showLoginModal) : console.error('loginButton is null');
-    closeModal ? closeModal.addEventListener('click', hideLoginModal) : console.error('closeModal is null');
-    window.addEventListener('click', (event) => event.target == loginModal ? loginModal.style.display == "none" : null);
+    loginButton ? loginButton.addEventListener('click', toggleLoginModal) : console.error('loginButton is null');
+    closeModal ? closeModal.addEventListener('click', closeLoginModal) : console.error('closeModal is null');
+
+    if (overlay) {
+        overlay.addEventListener('click', (event) => {
+            if (event.target === overlay) {
+                closeLoginModal();
+            }
+        });
+    } else {
+        console.error("overlay is null");
+    }
 });
