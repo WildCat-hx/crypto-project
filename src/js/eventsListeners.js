55ffd2bc0052b7bfea60d35e17c6f7eba476d5df
WildCat-hx <salomecadillon46@gmail.com>
2024-09-13 01:54:43 +0200

    v2-dev : refactor et feat - refactor : regroupement des events listeners dans fonction init - feat : methode addClickListeners facilitant l'ajout d'events listeners

--- src/js/eventsListeners.js
+++ src/js/eventsListeners.js
@@ -8,22 +8,32 @@ import {
     scrollFunct, 
     toggleLoginModal,
     closeLoginModal,
-    overlay
+    overlay,
+    updateClassName,
+    debounce
 } from './methodes.js'
 
-document.addEventListener("DOMContentLoaded", function() {
-    navbarWhite ? window.addEventListener('scroll', scrollFunct) : console.error("navbarWhite is null");
-    loginButton ? loginButton.addEventListener('click', toggleLoginModal) : console.error('loginButton is null');
-    loginButtonFooter ? loginButtonFooter.addEventListener('click', toggleLoginModal) : console.error('loginButton is null');
-    closeModal ? closeModal.addEventListener('click', closeLoginModal) : console.error('closeModal is null');
+document.addEventListener("DOMContentLoaded", initEventListeners);
 
-    if (overlay) {
-        overlay.addEventListener('click', (event) => {
-            if (event.target === overlay) {
-                closeLoginModal();
-            }
-        });
+function initEventListeners() {
+
+    addClickListener(loginButton, toggleLoginModal, 'loginButton');
+    addClickListener(loginButtonFooter, toggleLoginModal, 'loginButtonFooter');
+    addClickListener(closeModal, closeLoginModal, 'closeModal');
+    navbarWhite ? window.addEventListener('scroll', scrollFunct) : console.error("navbarWhite is null");
+    window.addEventListener('resize', debounce(updateClassName, 200));
+
+    overlay.addEventListener('click', (event) => {
+        if (event.target === overlay) {
+            closeLoginModal();
+        }
+    });
+}
+
+function addClickListener(element, handler, elementName) {
+    if (element) {
+        element.addEventListener('click', handler);
     } else {
-        console.error("overlay is null");
+        console.error(`${elementName} is null`);
     }
-});
+}
